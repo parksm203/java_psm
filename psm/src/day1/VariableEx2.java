64d22c2b52950049686bf3958cf544cc76511656
parksm203 <93233427+parksm203@users.noreply.github.com>
2021-10-27 16:50:02 +0900

    1-3 : 변수명 작성 규칙 예제

---
+++ psm/src/day1/VariableEx2.java
@@ -0,0 +1,21 @@
+package day1;
+
+public class VariableEx2 {
+	//변수명 작성 규칙과 관례 예제
+	public static void main(String[] args) {
+
+		int num1 = 0;//가능
+		//int 1num = 0;//에러발생. 숫자로 시작불가
+		//int num1 = 1;//에러발생. 중복선언 불가
+		//int _; //에러발생. _만 사용가능
+		int abc_;//가능
+		//int abc^; //에러발생. _와 $를 제외한 특수문자 불가능
+		int 변수;//가능 .잘사용하지않음
+		//int int // 에러발생. 키워드 사용불가
+		int Num1 = 0;//가능. 대소문자 구분
+		
+	
+	}
+	
+
+}
