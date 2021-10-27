ee939be8e4c73044188a45ce1da3dc1ba5f2768c
parksm203 <93233427+parksm203@users.noreply.github.com>
2021-10-27 16:51:11 +0900

    1-5 : 문자열 선언 예제

---
+++ psm/src/day1/stringEx1.java
@@ -0,0 +1,18 @@
+package day1;
+
+public class stringEx1 {
+
+	public static void main(String[] args) {
+		/* 문자영 string
+		 * */
+		String str1;
+		String str2 = "Hello";
+		System.out.println(str2);
+		str1 = str2 + " world ";
+		System.out.println(str1);
+		
+		
+
+	}
+
+}
