443728209abf1387ab81c3492170137dd99c1fd7
WaitingDW <bestoboist23@gmail.com>
2021-10-28 17:49:47 +0900

    2-12 : if else문 홀짝 예제

---
+++ psm/src/day2/IfEvenEx2.java
@@ -0,0 +1,26 @@
+package day2;
+
+public class IfEvenEx2 {
+
+	public static void main(String[] args) {
+		/* num1가 짝수이면 짝수라고 출력하는 코드를 작성하세요.
+		 * 조건식 : num1가 짝수이다 
+		 * 		num가 2의 배수이다
+		 * 		num를 2로 나누었을때 나머지가 0이다
+		 * 		num를 2로 나누었을때 나머지가 0과 같다
+		 * 실행문 : 짝수라고 출력
+		 * */
+		int num = 11;
+		if(num % 2 ==0) {
+			System.out.println(num + "는 짝수");
+		}else {
+			System.out.println(num + "는 홀수");
+			
+			
+		
+		
+		
+		
+		}	
+	}
+}
