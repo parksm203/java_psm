6a075906a7734f1b855e3203ecec6669c3af91b9
psm203 <parksm203@naver.com>
2021-11-01 17:16:31 +0900

    4-9 : for문 break문 예제

---
+++ psm/src/day4/ForBreakEx1.java
@@ -0,0 +1,36 @@
+package day4;
+
+import java.util.Scanner;
+
+public class ForBreakEx1 {
+
+	public static void main(String[] args) {
+		/*0은 입력할 때까지 정수를 계속 입력받는 코드를 설정하세요 .
+		 * break문을 이용.
+		 * 예)
+		 * 정수를 입력하세요 : 1
+		 * 정수를 입력하세요 : 1000
+		 * 정수를 입력하세요 : -1
+		 * 정수를 입력하세요 : -123
+		 * 정수를 입력하세요 : 0
+		 * 종료됩니다.
+		 * ---힌트---
+		 * Scanner는 반복문 시작전에 만들어야 함.
+		 * 정수를 입력받는 코드를 반복문 안에 있어야 함.
+		 * 입력받은 정수가 0과 같으이면 반복문을 빠져나옴.
+		 * */
+		Scanner scan = new Scanner(System.in);
+		int num;
+		for( ; ; ) {
+			System.out.println("정수를 입력하세요 : ");
+			num = scan.nextInt();
+			if(num == 0) {
+				break;
+			}
+		}
+		System.out.println("프로그램을 종료합니다.");
+		scan.close();
+	
+	}
+
+}
