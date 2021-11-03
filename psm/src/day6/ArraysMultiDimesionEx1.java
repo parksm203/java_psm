07983d2c6f58ca2efda2ca45ba895b32272626bc
psm203 <parksm203@naver.com>
2021-11-03 17:24:28 +0900

    6-5 : 다차원 배열 예제

---
+++ psm/src/day6/ArraysMultiDimesionEx1.java
@@ -0,0 +1,19 @@
+package day6;
+
+public class ArraysMultiDimesionEx1 {
+
+	public static void main(String[] args) {
+		/*한 학년의 성적을 관리하기 위해 이차원 배열을 선언 및 생성하세요.
+		 *한 학년은 최대 10반까지 있고, 학 번에 최대 30명까지 배치
+		 *성적은 정수*/ 
+		int [][] score = new int [10][30];
+		//1반 30번의 성적에 80을 입력
+		score[0][29] = 80;
+		//고등학교 모든 학년의 성적을 관리하기 위한 배열 선언
+		int [][][] highscore = new int [3][10][30];
+		
+		 
+		
+	}
+
+}
