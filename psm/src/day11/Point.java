bebcc4603a0fad7c807917eca23fc8b1852de2d4
parksm203 <93233427+parksm203@users.noreply.github.com>
2021-11-10 22:47:12 +0900

    11-1 : 클래스 사각형 예제

---
+++ psm/src/day11/Point.java
@@ -0,0 +1,42 @@
+package day11;
+
+//2차원 좌표 평면의 점 하나를 나타내는 클래스
+public class Point {
+	private int x,y;
+
+	public int getX() {
+		return x;
+	}
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public void setY(int y) {
+		this.y = y;
+	}
+
+	public Point(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+	
+
+public Point(Point lt) {
+		// TODO Auto-generated constructor stub
+	}
+
+//좌표 이동 기능
+public void move (int x, int y) {
+	this.x = x;
+	this.y = y;	
+	}
+	//좌표를 출력하는 기능
+	public void print() {
+		System.out.println("(" + x + " , " + y + ")");
+	}
+}
