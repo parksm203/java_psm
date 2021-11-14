7d161949a5d72ae5347349f41b4b4e2a5895964c
parksm203 <93233427+parksm203@users.noreply.github.com>
2021-11-15 00:00:02 +0900

    13-1 : 추상 클래스 도형 예제

---
+++ psm/src/day13/ExbEllipse.java
@@ -0,0 +1,8 @@
+package day13;
+
+
+public class ExbEllipse extends ExbShape {
+	public void print() {
+		System.out.println("타원");
+	}
+}
---
+++ psm/src/day13/ExbRect.java
@@ -0,0 +1,11 @@
+package day13;
+
+public class ExbRect extends ExbShape {
+
+	public void print() {
+	
+	 System.out.println("사각형");
+		
+
+	}
+}
